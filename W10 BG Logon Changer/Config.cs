c5e8d9349e80399816f5671b5c235fa9d1507ad6
Toyz <toyzz@live.com>
2015-07-30 17:27:02 -0700

    Sexy C# Build Time
    Made a working C# build requires nuget :dart:

---
+++ W10 BG Logon Changer/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace W10_BG_Logon_Changer
+{
+    public static class Config
+    {
+        public static readonly string PriFileName = "Windows.UI.Logon.pri";
+        public static readonly string BakPriFileName = PriFileName + ".bak";
+
+        public static readonly string LogonFolder = Path.Combine(Environment.GetEnvironmentVariable("windir"), "SystemResources", "Windows.UI.Logon");
+        public static readonly string PriFileLocation = Path.Combine(LogonFolder, PriFileName);
+        public static readonly string BakPriFileLocation = Path.Combine(LogonFolder, BakPriFileName);
+    }
+}
---
+++ W10 BG Logon Changer/Extenstions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MColor = System.Windows.Media.Color;
+using DColor = System.Drawing.Color;
+
+namespace W10_BG_Logon_Changer
+{
+    public static class Extenstions
+    {
+        public static MColor ToMediaColor(this DColor color)
+        {
+            return MColor.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
