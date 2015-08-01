0549cc5ebc70dfc48bd94c5ee7377b10a97d2418
Toyz <toyzz@live.com>
2015-07-31 21:23:59 -0700

    Changed fadespped

--- W10 BG Logon Changer/Tools/Animations/ImageFader.cs
+++ W10 BG Logon Changer/Tools/Animations/ImageFader.cs
@@ -6,14 +6,16 @@ namespace W10_BG_Logon_Changer.Tools.Animations
 {
     public static class ImageFader
     {
-        private static int _speed = 1;
+        private const int Speed = 1;
+        private const double Fadespeed = 0.10;
+
         public async static void Fadein(Image image)
         {
-            for (double i = 0; i < 1.01; i += 0.01)
+            for (double i = 0; i < 1.01; i += Fadespeed)
             {
                 i = Math.Round(i, 2);
 
-                await Task.Delay(_speed);
+                await Task.Delay(Speed);
 
                 image.Opacity = i;
             }
@@ -21,11 +23,11 @@ namespace W10_BG_Logon_Changer.Tools.Animations
 
         public async static void Fadeout(Image image)
         {
-            for (double i = 1; i > -0.01; i -= 0.01)
+            for (double i = 1; i > -0.01; i -= Fadespeed)
             {
                 i = Math.Round(i, 2);
 
-                await Task.Delay(_speed);
+                await Task.Delay(Speed);
 
                 image.Opacity = i;
             }
