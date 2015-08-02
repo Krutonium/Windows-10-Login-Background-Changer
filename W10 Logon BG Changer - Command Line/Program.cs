e3e66f878b443369c41dc7295083b749aab0d2af
Syrexide <wwc.302@gmail.com>
2015-08-02 17:16:18 -0400

    added features to command line version

--- W10 Logon BG Changer - Command Line/Program.cs
+++ W10 Logon BG Changer - Command Line/Program.cs
@@ -12,10 +12,24 @@ namespace W10_Logon_BG_Changer___Command_Line
 
         private static void Main(string[] args)
         {
+
+            switch (args[0])
+            {
+                case "authors":
+                    Console.WriteLine("");
+                    Console.WriteLine(@"Syrexide | https://github.com/Syrexide/");
+                    Console.WriteLine(@"Toyz | https://github.com/Toyz/");
+                    return;
+
+                case "restore":
+                    Restore();
+                    return;
+            }
+
             if (args.Length == 1)
             {
                 var filedir = args[0];
-                if (File.Exists(filedir) && (Path.GetExtension(filedir) == ".jpg"))
+                if (File.Exists(filedir) && (Path.GetExtension(filedir) == ".jpg") || (Path.GetExtension(filedir) == ".png") || (Path.GetExtension(filedir) == ".bmp") || (Path.GetExtension(filedir) == ".tif") || (Path.GetExtension(filedir) == ".tiff"))
                 {
                     ChangeImage(filedir);
                 }
@@ -28,7 +42,7 @@ namespace W10_Logon_BG_Changer___Command_Line
             else
             {
                 Console.WriteLine("");
-                Console.WriteLine(@"Please format your command as shown: commandthing [image path]");
+                Console.WriteLine(@"Please format your command as shown: commandthing [imagepath]");
                 Console.WriteLine("");
                 Console.WriteLine(@"Press any key to exit.");
                 Console.ReadKey();
@@ -38,6 +52,7 @@ namespace W10_Logon_BG_Changer___Command_Line
         private static void ChangeImage(string filedir)
         {
             HelperLib.TakeOwnership(Config.LogonFolder);
+
             HelperLib.TakeOwnership(Config.PriFileLocation);
 
             if (!File.Exists(Config.BakPriFileLocation))
@@ -62,6 +77,12 @@ namespace W10_Logon_BG_Changer___Command_Line
             PriBuilder.CreatePri(TempPriFile, NewPriLocation, filedir);
 
             File.Copy(NewPriLocation, Config.PriFileLocation, true);
+        }
+
+        private static void Restore()
+        {
+            HelperLib.TakeOwnership(Config.PriFileLocation);
+            File.Copy(Config.BakPriFileLocation, Config.PriFileLocation, true);
         }
     }
 }
