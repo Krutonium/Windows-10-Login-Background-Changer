ba66f39d82d66c0cf9d2d7668389955d1fb8e4e3
Toyz <toyzz@live.com>
2015-08-02 21:48:34 -0700

    Current.img moving

--- W10 BG Logon Changer/MainWindow.xaml.cs
+++ W10 BG Logon Changer/MainWindow.xaml.cs
@@ -85,7 +85,12 @@ namespace W10_BG_Logon_Changer
                 var temp = Path.GetTempFileName();
 
                 File.Copy(Config.CurrentImageLocation, temp, true);
-                WallpaperViewer.Source = new BitmapImage(new Uri(temp));
+
+                Settings.Default.Set("current.img", File.ReadAllBytes(temp));
+                Settings.Default.Save();
+
+                File.Delete(Config.CurrentImageLocation);
+                //WallpaperViewer.Source = new BitmapImage(new Uri(temp));
             }
 
             Loaded += (o, i) =>
@@ -93,6 +98,15 @@ namespace W10_BG_Logon_Changer
                 SettingFlyout.Position = Settings.Default.Get("flyout", Position.Right);
                 GlyphsViewer.ToolTip = Settings.Default.Get("filename", "No File");
             };
+
+            if (Settings.Default.Exist("current.img"))
+            {
+                var temp = Path.GetTempFileName();
+
+                File.WriteAllBytes(temp, Settings.Default.Get<byte[]>("current.img"));
+
+                WallpaperViewer.Source = new BitmapImage(new Uri(temp));
+            }
         }
 
         public string SelectedFile
@@ -148,7 +162,10 @@ namespace W10_BG_Logon_Changer
                 File.Delete(_newPriLocation);
             }
 
-            File.Copy(SelectedFile, Config.CurrentImageLocation, true);
+            //File.Copy(SelectedFile, Config.CurrentImageLocation, true);
+
+            Settings.Default.Set("current.img", File.ReadAllBytes(SelectedFile));
+            Settings.Default.Save();
 
             File.Copy(Config.BakPriFileLocation, _tempPriFile, true);
 
