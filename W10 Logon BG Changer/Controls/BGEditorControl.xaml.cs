502a6e9e06f848ebccc22c45465b29bc5e52f658
Toyz <toyzz@live.com>
2015-08-03 14:35:48 -0700

    Made it delete current.img from settings

--- W10 Logon BG Changer/Controls/BGEditorControl.xaml.cs
+++ W10 Logon BG Changer/Controls/BGEditorControl.xaml.cs
@@ -130,8 +130,8 @@ namespace W10_Logon_BG_Changer.Controls
             if (msg != MessageBoxResult.Yes) return;
             File.Copy(Config.BakPriFileLocation, Config.PriFileLocation, true);
 
-            File.Delete(Config.CurrentImageLocation);
-
+            //File.Delete(Config.CurrentImageLocation);
+            
             var f = Path.GetTempFileName();
             Properties.Resources._default.Save(f, ImageFormat.Png);
 
@@ -224,6 +224,8 @@ namespace W10_Logon_BG_Changer.Controls
             if (image != "")
             {
                 OverrideImageFromMainWindow(image);
+                Settings.Default.Delete("current.img");
+                Settings.Default.Save();
             }
         }
 
