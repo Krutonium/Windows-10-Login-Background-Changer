94addeaa08fb285666d638ac3d458b3b7e912739
Toyz <toyzz@live.com>
2015-08-04 11:22:08 -0700

    Added basic language support :D

---
+++ LangaugeLibrary/XmlToDynamic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace LanguageLibrary
+{
+    public class XmlToDynamic
+    {
+        public static void Parse(dynamic parent, XElement node)
+        {
+            if (node.HasElements)
+            {
+                if (node.Elements(node.Elements().First().Name.LocalName).Count() > 1)
+                {
+                    //list
+                    var item = new ExpandoObject();
+                    var list = new List<dynamic>();
+                    foreach (var element in node.Elements())
+                    {
+                        Parse(list, element);
+                    }
+
+                    AddProperty(item, node.Elements().First().Name.LocalName, list);
+                    AddProperty(parent, node.Name.ToString(), item);
+                }
+                else
+                {
+                    var item = new ExpandoObject();
+
+                    foreach (var attribute in node.Attributes())
+                    {
+                        AddProperty(item, attribute.Name.ToString(), attribute.Value.Trim());
+                    }
+
+                    //element
+                    foreach (var element in node.Elements())
+                    {
+                        Parse(item, element);
+                    }
+
+                    AddProperty(parent, node.Name.ToString(), item);
+                }
+            }
+            else
+            {
+                AddProperty(parent, node.Name.ToString(), node.Value.Trim());
+            }
+        }
+
+        private static void AddProperty(dynamic parent, string name, object value)
+        {
+            var list = parent as List<dynamic>;
+            if (list != null)
+            {
+                list.Add(value);
+            }
+            else
+            {
+                var dictionary = parent as IDictionary<String, object>;
+                if (dictionary != null)
+                    dictionary[name] = value;
+            }
+        }
+    }
+}
