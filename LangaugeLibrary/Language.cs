275896cb9827ecbc72e492612d3ea12ff3ef8db0
Toyz <toyzz@live.com>
2015-08-04 11:22:28 -0700

    Added untracked files >_>

---
+++ LangaugeLibrary/Language.cs
@@ -0,0 +1,29 @@
+﻿using System.Dynamic;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace LanguageLibrary
+{
+    public class Language
+    {
+        public static dynamic Default { get; private set; }
+
+        public static void Set(string name)
+        {
+            dynamic temp = new ExpandoObject();
+            string xml = "";
+
+            using (Stream stream = Assembly.GetAssembly(typeof(Language)).GetManifestResourceStream("LangaugeLibrary.Langs." + name + ".xml"))
+                if (stream != null)
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        xml = reader.ReadToEnd();
+                    }
+
+            XmlToDynamic.Parse(temp, XElement.Parse(xml));
+
+            Default = temp.Language;
+        }
+    }
+}
