a94468a9b463702eda1e60cc42a5f596f5500bdc
Toyz <toyzz@live.com>
2015-08-06 18:46:38 -0700

    Minor updates to code

---
+++ W10 Logon BG Changer - Command Line/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace W10_Logon_BG_Changer___Command_Line
+{
+    public static class Extensions
+    {
+        public static bool IsHex(this string hex)
+        {
+            return (Regex.Match(hex, "^#(?:[0-9a-fA-F]{3}){1,2}$").Success);
+        }
+    }
+}
