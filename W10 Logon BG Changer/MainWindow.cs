68058bef1da08e4289348e3b649b0674011275be
Toyz <toyzz@live.com>
2015-08-19 09:47:40 -0700

    Fixed Language spelling bug in title_bg_disabled

--- W10 Logon BG Changer/MainWindow.cs
+++ W10 Logon BG Changer/MainWindow.cs
@@ -74,7 +74,7 @@ namespace W10_Logon_BG_Changer
             if (Helpers.IsBackgroundDisabled())
             {
                 WpfMessageBox.Show(LanguageLibrary.Language.Default.background_disabled,
-                    LanguageLibrary.Language.Default.title_dg_disabled, MessageBoxButton.OK, MessageBoxImage.Information);
+                    LanguageLibrary.Language.Default.title_bg_disabled, MessageBoxButton.OK, MessageBoxImage.Information);
             }
 
             Debug.WriteLine("[AccentColor]: " +
